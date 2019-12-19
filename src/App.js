f07a8430f29fc5e3c8a7c0e6c89948cccc281cb4
Hector Valdez <hvaldez180@gmail.com>
2019-12-19 18:18:56 -0500

    saving to deploy

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     <ProductContext.Provider value={{ products, addItem }}>
       <CartContext.Provider value={cart}>
         <div className="App">
-          <Navigation cart={cart} />
+          <Navigation />
 
           {/* Routes */}
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -8,18 +8,21 @@ import Product from './Product';
 
 const Products = () => {
 const { products, addItem } = useContext(ProductContext);
+
 	return (
 		<div className="products-container">
 
 			{products.map(product => (
+				
 				<Product
 					key={product.id}
 					product={product}
 					addItem={addItem}
 				/>
+				
 			))}
 		</div>
 	);
 };
-
+// console.log(product,"product")
 export default Products;
